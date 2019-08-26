4073eda7fc62f5935f2ac71650ddb39eaf1be0f6
Matt <matt@mspacecreative.com>
2019-08-26 07:05:30 -0300

    c

--- wp-content/themes/stratusx-child/includes/sonacast.php
+++ wp-content/themes/stratusx-child/includes/sonacast.php
@@ -10,6 +10,8 @@
 		flex-basis: 32%;
 		margin-bottom: 2%;
 		margin-right: 2%;
+		position: relative;
+		box-shadow: 0 0 15px #c9c9c9;
 	}
 	.sonacast-episode:nth-child(3n) {
 		margin-right: 0;
@@ -17,6 +19,21 @@
 	.sonacast-episode iframe {
 		width: 100%;
 		display: block;
+	}
+	.sonacast-episode span {
+		font-weight: bold;
+		position: absolute;
+		top: 0;
+		left: 0;
+		font-size: 15px;
+		padding: 10px 15px;
+		border-radius: 0 0 5px 0;
+		background-color: #000;
+		color: #fff;
+	}
+	.sonacast-episode h3 {
+		line-height: 1.25em;
+		font-size: 20px;
 	}
 	@media (max-width: 980px) {
 		.sonacast-episode {
@@ -50,10 +67,13 @@ if( have_rows('episodes') ): ?>
  	<?php while ( have_rows('episodes') ) : the_row(); ?>
 
         <div class="sonacast-episode">
-			<h3><span style="font-weight: bold;"><?php esc_html_e('Episode #'); ?><?php the_sub_field('episode_#'); ?></span> <?php the_sub_field('title'); ?></h3>
-			<audio controls>
-				<source src="<?php the_sub_field('episode'); ?>">
-			</audio>
+			<div class="inner">
+				<span style="font-weight: bold;"><?php esc_html_e('Episode #'); ?><?php the_sub_field('episode_#'); ?></span>
+				<h3><?php the_sub_field('title'); ?></h3>
+				<audio controls>
+					<source src="<?php the_sub_field('episode'); ?>">
+				</audio>
+			</div>
 		</div>
 
     <?php endwhile;
