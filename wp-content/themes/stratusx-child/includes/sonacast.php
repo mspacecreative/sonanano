289ad83fd16e357574a243e831c51271f3cd2817
Matt <matt@mspacecreative.com>
2019-08-28 09:28:38 -0300

    c

--- wp-content/themes/stratusx-child/includes/sonacast.php
+++ wp-content/themes/stratusx-child/includes/sonacast.php
@@ -119,7 +119,7 @@ array_multisort( $order, SORT_DESC, $repeater );
 
         <div class="sonacast-episode">
 			<div class="inner">
-				<span style="font-weight: bold;"><?php esc_html_e('EP'); ?><?php echo get_row_index(); ?></span>
+				<span style="font-weight: bold;"><?php esc_html_e('EP'); ?><?php echo $row['id']; ?></span>
 				<h3><?php echo $row['title']; ?></h3>
 				
 				<?php if ( get_sub_field('synopsis') ): ?>
