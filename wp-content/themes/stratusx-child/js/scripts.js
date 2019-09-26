1a0cbfe76bad508339d7dbef747e8d882b1c36d4
Matt <matt@mspacecreative.com>
2019-09-26 13:47:48 -0300

    c

--- wp-content/themes/stratusx-child/js/scripts.js
+++ wp-content/themes/stratusx-child/js/scripts.js
@@ -1,7 +1,28 @@
+jQuery(window).load(function() {
+	jQuery('.yl-focus').unwrap();
+});
+
 (function($) {
-	
+	function getId(url) {
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+
+    if (match && match[2].length == 11) {
+        return match[2];
+    } else {
+        return 'error';
+    }
+}
+
+var myId;
+
+$(document).ajaxSuccess(function() {
 	$('.yl-focus').each(function() {
-		$(this).unwrap();
+		var myUrl = $(this).attr('href');
+		myId = getId(myUrl);
+
+		$(this).after('<div class="video-container"><iframe width="560" height="315" src="//www.youtube.com/embed/' + myId + '" frameborder="0" allowfullscreen></iframe></div>');
 	});
+});
 	
 })(jQuery);
